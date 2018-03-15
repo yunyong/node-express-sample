5def3d993cd686bd434cd59e28fd90c6135cef77
ibra.z <ibra.z@kakaocorp.com>
2018-03-15 13:55:39 +0900

    fs write file

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const bodyParser = require('body-parser');
 
 app.locals.pretty = true;
 
@@ -8,21 +9,34 @@ app.set('view engine', 'jade');
 app.set('views', './templates')
 
 app.use(express.static('static'));
+app.use(bodyParser.urlencoded({
+    extended : false
+}));
 
 app.listen(3000, () => {
     console.log(`Server running at http://127.0.0.1:3000/`);
 });
 
 app.get('/', (req, res) => {
-    res.render('index', {
-        title : 'welcome'
-    });
+    res.redirect('/topic');
 });
 
-app.get('/topic/:name', (req, res) => {
+app.get('/topic/create', (req, res) => {
+    res.render('form');
+});
+
+app.get(['/topic','/topic/:name'], (req, res) => {
     const name = req.params.name;
 
     const callback = (list) => {
+        if(!name){
+            res.render('topic', {
+                title : 'topic list',
+                list : list,
+                topic : '',
+                desc : ''
+            });
+        };
         fs.readFile(`data/${name}.txt`, 'utf8', (err, data) => {
             if(err){
                 console.log(err);
@@ -44,5 +58,18 @@ app.get('/topic/:name', (req, res) => {
         };
         callback(files);
     })
-
 });
+
+app.post('/topic', (req, res) => {
+    const title = req.body.title;
+    const desc = req.body.desc;
+
+    fs.writeFile(`data/${title}.txt`, desc, (err) => {
+        if(err){
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        };
+        res.redirect(`/topic/${title}`);
+    });
+});
+
